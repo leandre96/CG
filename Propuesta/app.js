61f5d96ba84f7addee12af22e1198c8d62f02d27
leandre96 <leo_silver12@hotmail.com>
2018-11-29 08:17:42 -0500

    changes in app.js

--- Propuesta/app.js
+++ Propuesta/app.js
@@ -1,27 +1,28 @@
-var scene = new THREE.Scene();
-var backgroundScene = "#40E0D0"
-scene.background = new THREE.Color(backgroundScene);
-var camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,500);
-var renderer = new THREE.WebGLRenderer();
-var selected_object = null;
-renderer.setSize(window.innerWidth,window.innerHeight);
-document.body.appendChild(renderer.domElement);
-var mouseOrbit = new THREE.OrbitControls(camera, renderer.domElement);
-var getMaterial = function(colorDesired){
+var scene = new THREE.Scene();//Se crea una escena de Three.js
+var backgroundScene = "#40E0D0"//Se define un color de fondo para la escena
+scene.background = new THREE.Color(backgroundScene);//Se setea dicho color para la escena
+var camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,500);//Se define una cámara
+var renderer = new THREE.WebGLRenderer();//Se define un renderizador
+var selected_object = null;//Variable para elementos seleccionados
+renderer.setSize(window.innerWidth,window.innerHeight);//Se setea el tamaño al renderizador
+document.body.appendChild(renderer.domElement);//Se procede a añadir al renderizador al DOM
+var mouseOrbit = new THREE.OrbitControls(camera, renderer.domElement);//Se añade un controlador de órbita
+var getMaterial = function(colorDesired){//
     let material = new THREE.MeshPhongMaterial({ color:colorDesired, side: THREE.DoubleSide, flatShading: true });
     return material;}
-var tablero = new THREE.Group();
+var tablero = new THREE.Group();//Se define un grupo para el tablero de ajedrez
 var tile_width=2;
 var tile_color="#ffffff";
-var addTablero = function(){
+var addTablero = function(){//Función que se encargara de agregar elementos a la tablero de ajedrez
     var tile_geometry = new THREE.BoxGeometry(tile_width,tile_width/10,tile_width);
-    var black_material = getMaterial("#000000");   //fixed color
-    var color_material = getMaterial(tile_color);  //to change with gui
+    var black_material = getMaterial("#000000");   //Color negro fijo
+    var color_material = getMaterial("#FFB6C1");  //to change with gui
     var black_color = -1;
-    for(var i = 0; i < 8; i++){
+	var length = 10; //Cantidad de cuadros de lado a lado
+    for(var i = 0; i < length; i++){
         if (i % 2 == 0) { black_color = -1 }
         else { black_color = 1 };
-        for(var j = 0; j < 8; j++){
+        for(var j = 0; j < length; j++){
             var material;
             if(black_color == 1){ material = black_material; }   // iterate black and white
             else{ material = color_material; }
@@ -45,7 +46,7 @@ var addLights = function( distanceFromCenter ){
     white_down_light = getSpotLight("#ffffff",1);
     white_down_light.position.set(0 - tile_width, -20, 0 - tile_width);
     scene.add(white_down_light);
-    red_light = getSpotLight("#ff0000",6);
+    red_light = getSpotLight("#FF1493",6);
     red_light.position.set(distanceFromCenter - tile_width, 20, distanceFromCenter - tile_width);
     scene.add(red_light);
     green_light = getSpotLight("#00ff00",6);
@@ -54,7 +55,7 @@ var addLights = function( distanceFromCenter ){
     blue_light = getSpotLight("#0000ff",6);
     blue_light.position.set( 0 - tile_width, 20, -distanceFromCenter - tile_width);
     scene.add(blue_light); }
-var prisma, esfera, piramide, toroide;
+var prism, esfera, piramide, toroide;
 var figurasCreadas = false;
 var addFiguras = function(){
     //piramide, la diferencia entre cylinder y cylinder 
@@ -63,30 +64,31 @@ var addFiguras = function(){
     let geometry = new THREE.CylinderBufferGeometry(0,2,3,4);
     let material = getMaterial("#800080");
     piramide = new THREE.Mesh(geometry,material);
-    piramide.position.set(-3.5,7,-3);
-    scene.add(piramide);   
-    //prisma de n lados es un cilindro si n es lo suficientemente grande 
-    geometry = new THREE.CylinderGeometry(1,1,3,8);
-    material = getMaterial("#A52A2A");
-    prisma = new THREE.Mesh(geometry,material);
-    prisma.position.set(2.5,5.5,-3);
-    scene.add(prisma);
-    // Torus - Toroide
+    piramide.position.set(-4,2,3);	
+    scene.add(piramide);
+	
+	geometry = new THREE.SphereBufferGeometry(1.5,32,32);
+    material = getMaterial("#777777");
+    esfera = new THREE.Mesh(geometry,material);
+    esfera.position.set(2,3,3);	
+    scene.add(esfera);	
+
     geometry = new THREE.TorusKnotGeometry(1.5,0.5,32,100);
     material = getMaterial("#ADFF2F");   //fixed color
     toroide = new THREE.Mesh(geometry,material);
-    toroide.position.set(2,3,3);
+    toroide.position.set(2.5,5.5,-3);	
     toroide.rotation.x = Math.PI / 2;
     scene.add(toroide);
-    //esfera
-    geometry = new THREE.SphereBufferGeometry(1.5,32,32);
-    material = getMaterial("#777777");
-    esfera = new THREE.Mesh(geometry,material);
-    esfera.position.set(-4,2,3);
-    scene.add(esfera); 
+    
+    geometry = new THREE.CylinderGeometry(1,1,3,8);
+    material = getMaterial("#A52A2A");
+    prism = new THREE.Mesh(geometry,material);
+    prism.position.set(-3.5,7,-3);
+	
+    scene.add(prism);	
     figurasCreadas = true; }
 var removeFiguras = function(){
-    scene.remove(prisma);
+    scene.remove(prism);
     scene.remove(esfera);
     scene.remove(piramide);
     scene.remove(toroide); 
@@ -97,16 +99,14 @@ var rotateFigura = function(figura,vel_X,vel_Y,vel_Z){
     if(vel_Z != 0){ figura.rotation.z += vel_Z; } }
     var rotationOn = function(){
         rotateFigura(piramide,0,params.speed,0); 
-        rotateFigura(prisma,0,params.speed,0);
+        rotateFigura(prism,0,params.speed,0);
         rotateFigura(toroide,0,params.speed,0);
         rotateFigura(esfera,0,0,params.speed); }
 var loader = new THREE.OBJLoader();
 loader.load('obj/Chinese_dragon.obj',function ( object ) {iron_man = object;object.scale.set(0.1,0.1,0.1);scene.add( object );
     object.add(pivotPoint);scene.add(pivotPoint);},
-    // called when loading is in progresses
     function ( xhr ) {console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );},
-    // called when loading has errors
-    function ( error ) {console.log( 'An error happened' );}
+    function ( error ) {console.log( 'Ocurrio una falla' );}
 );
 // GUI
 var params = { up_light: true, down_light: false, background: backgroundScene, 
@@ -135,10 +135,8 @@ var shapectl = gui.addFolder("Shape control");
 var shape_params = {
     color: "#00ffff",
 };
-
 // adding folder to gui control
 shapectl.addColor(shape_params, 'color').onChange(ChangeColor).listen();
-
 // instantiate raycaster 
 var raycaster = new THREE.Raycaster();
 var mouse = new THREE.Vector2();
@@ -154,11 +152,6 @@ document.addEventListener( 'mousedown', onDocumentMouseDown );
 function ChangeColor(){
     selected_object.material.color.setHex(shape_params.color);
 };
-
-// var dragcontrols = new THREE.DragControls( scene.children, camera, renderer.domElement );
-// dragcontrols.addEventListener( 'dragstart', function ( event ) { controls.enabled = false; } ); //cuando hace click sobre una figura y mientras la tiene sostenida
-// dragcontrols.addEventListener( 'dragend', function ( event ) { controls.enabled = true; } );  //cuando suelta una figura
-
 // callback event for mouse down
 function onDocumentMouseDown( event ) {    
             event.preventDefault();
